72b3b975bcf39bf7db81b1f885cd1c266be57f61
Aleksei Starkov <alexey.starkov@taskretail.com.au>
2020-04-21 23:16:51 +0200

    MainPage scroll interaction has been implemented

--- StarkovInteractiveCV/VisualElements/Pages/MainPage/MainPage.xaml.cs
+++ StarkovInteractiveCV/VisualElements/Pages/MainPage/MainPage.xaml.cs
@@ -4,9 +4,40 @@ namespace StarkovInteractiveCV.VisualElements.Pages.MainPage
 {
     public partial class MainPage : ContentPage
     {
+        private const double HeaderGridMinHeight = 80;
+        private const double HeaderGridHeightFactor = 0.4;
+
+        private double _headerGridHeightDefault;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnParentSet()
+        {
+            SizeChanged += (s, e) =>
+            {
+                if (Height > 0)
+                {
+                    HeaderGrid.HeightRequest = _headerGridHeightDefault = this.Height * HeaderGridHeightFactor;
+                    ScrollableStack.Padding = new Thickness(0, _headerGridHeightDefault, 0, 0);
+                }
+            };
+
+            Scroll.Scrolled += OnScrollScrolled;
+
+            base.OnParentSet();
+        }
+
+        private void OnScrollScrolled(object sender, ScrolledEventArgs e)
+        {
+            if (_headerGridHeightDefault - e.ScrollY < HeaderGridMinHeight)
+                HeaderGrid.HeightRequest = HeaderGridMinHeight;
+            else if(_headerGridHeightDefault - e.ScrollY >= _headerGridHeightDefault)
+                HeaderGrid.HeightRequest = _headerGridHeightDefault;
+            else
+                HeaderGrid.HeightRequest = _headerGridHeightDefault - e.ScrollY;
         }
     }
 }
