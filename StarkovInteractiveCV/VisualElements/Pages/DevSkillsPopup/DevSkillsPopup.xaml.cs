0967dc5532480ae65f462189add2f3392772c7ab
Aleksei Starkov <alexey.starkov@taskretail.com.au>
2020-05-03 00:01:09 +0200

    DevSkills popup part 1

---
+++ StarkovInteractiveCV/VisualElements/Pages/DevSkillsPopup/DevSkillsPopup.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Rg.Plugins.Popup.Pages;
+
+namespace StarkovInteractiveCV.VisualElements.Pages.DevSkillsPopup
+{
+    public partial class DevSkillsPopup : PopupPage
+    {
+        public DevSkillsPopup()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ StarkovInteractiveCV/VisualElements/Pages/DevSkillsPopup/DevSkillsPopupViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+using Prism.Navigation;
+using Prism.Services.Dialogs;
+using StarkovInteractiveCV.VisualElements.BaseObjects;
+using Xamarin.Forms;
+
+namespace StarkovInteractiveCV.VisualElements.Pages.DevSkillsPopup
+{
+    public class DevSkillsPopupViewModel : ViewModelBase
+    {
+        public ICommand CloseCommand => new Command(async (parameter) =>
+        {
+            await _navigationService.GoBackAsync();
+        });
+
+        public DevSkillsPopupViewModel(INavigationService navigationService, IDialogService dialogService)
+            : base(navigationService, dialogService)
+        {
+        }
+    }
+}
